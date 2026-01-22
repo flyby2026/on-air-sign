22cc06390bad57994166d8e320c31400eebd36d1
flyby2026 <ryanlutwin@gmail.com>
2026-01-22 18:22:27 -0500

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -15,21 +15,21 @@ const weeklySchedule = {
 
   // TUESDAY (2)
   2: [
-    { start: 0, end: 60, image: "images/DJ Nine - Nighttimes with Nine (Tuesday 12am - 1am).png" },
-    { start: 60, end: 120, image: "images/DJ BLUE - The Sorrow Hour (tuesday 1am-2am).png" },
-    { start: 120, end: 540, image: "images/OPEN STUDIO - 2AM-9AM.png" },
-    { start: 540, end: 600, image: "images/DJ Rocket - ROCKETFUEL (tuesday 9-10am).png" },
-    { start: 600, end: 660, image: "images/OPEN STUDIO - 10AM-11AM.png" },
-    { start: 660, end: 780, image: "images/Greg Keys - The Lunch rush (tuesday 11am-1pm).png" },
-    { start: 780, end: 840, image: "images/WIKD TRAINING SLOT - (1PM-2PM).png" },
-    { start: 840, end: 960, image: "images/DJ Haskell - The Soundscape (tuesday 2-4pm).png" },
-    { start: 960, end: 1080, image: "images/OPEN STUDIO - 4PM-6PM.png" },
-    { start: 1080, end: 1140, image: "images/DJ Whatsherface - the so and so show (tuesday 6-7pm).png" },
-    { start: 1140, end: 1200, image: "images/DJ Flieg - The Shakeup! (tuesday 7-8pm).png" },
-    { start: 1200, end: 1260, image: "images/DJ Player - 80s at 8 (tuesday 8-9pm).png" },
-    { start: 1260, end: 1320, image: "images/DJ Joe - Joe’s third set (tuesday 9-10pm).png" },
-    { start: 1320, end: 1380, image: "images/Dj keem - the altar (10-11pm).png" },
-    { start: 1380, end: 1440, image: "images/DJ Tia - the blue hour (11pm-12am).png" }
+    { start: 0, end: 60, image: "images/TUESDAY DJ Nine - Nighttimes with Nine (Tuesday 12am - 1am).png" },
+    { start: 60, end: 120, image: "images/TUESDAY DJ BLUE - The Sorrow Hour (tuesday 1am-2am).png" },
+    { start: 120, end: 540, image: "images/TUESDAY OPEN STUDIO - 2AM-9AM.png" },
+    { start: 540, end: 600, image: "images/TUESDAY DJ Rocket - ROCKETFUEL (tuesday 9-10am).png" },
+    { start: 600, end: 660, image: "images/TUESDAY OPEN STUDIO - 10AM-11AM.png" },
+    { start: 660, end: 780, image: "images/TUESDAY Greg Keys - The Lunch rush (tuesday 11am-1pm).png" },
+    { start: 780, end: 840, image: "images/TUESDAY WIKD TRAINING SLOT - (1PM-2PM).png" },
+    { start: 840, end: 960, image: "images/TUESDAY DJ Haskell - The Soundscape (tuesday 2-4pm).png" },
+    { start: 960, end: 1080, image: "images/TUESDAY OPEN STUDIO - 4PM-6PM.png" },
+    { start: 1080, end: 1140, image: "images/TUESDAY DJ Whatsherface - the so and so show (tuesday 6-7pm).png" },
+    { start: 1140, end: 1200, image: "images/TUESDAY DJ Flieg - The Shakeup! (tuesday 7-8pm).png" },
+    { start: 1200, end: 1260, image: "images/TUESDAY DJ Player - 80s at 8 (tuesday 8-9pm).png" },
+    { start: 1260, end: 1320, image: "images/TUESDAY DJ Joe - Joe’s third set (tuesday 9-10pm).png" },
+    { start: 1320, end: 1380, image: "images/TUESDAY Dj keem - the altar (10-11pm).png" },
+    { start: 1380, end: 1440, image: "images/TUESDAY DJ Tia - the blue hour (11pm-12am).png" }
   ]
 };
 
