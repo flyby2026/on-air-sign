26bbeac069fb7be9f4c8f463c02017bebef7a7d3
flyby2026 <ryanlutwin@gmail.com>
2026-02-17 14:25:14 -0500

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -17,7 +17,7 @@ const schedule = {
     { start: "18:00", end: "19:00", image: "MONDAY DJ L - Showcase (Monday 6-7 pm) (1).png" },
     { start: "19:00", end: "21:00", image: "MONDAY DJ Starman - The Flashback (monday 7-9 pm).png" },
     { start: "21:00", end: "22:00", image: "MONDAY DJ Desinova - The Cosmic Jam (monday 9-10 pm).png" },
-    { start: "22:00", end: "23:00", image: "MONDAY DJ biscuit- The Jam (monday 10-11 pm).png" },
+    { start: "22:00", end: "23:00", image: "MONDAY OPEN STUDIO - 10-11pm.png" },
     { start: "23:00", end: "24:00", image: "MONDAY DJ noli- holy cannoli (monday 11pm-12am).png" }
   ],
 
